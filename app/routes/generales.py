2fc1d5dff97819a9215bd5d87000d4f023d148bb
Juliette Benguigui <juliette.benguigui@hotmail.fr>
2024-12-15 17:33:08 +0100

    Construction de la route et du template (manque jinja)

--- app/routes/generales.py
+++ app/routes/generales.py
@@ -1,7 +1,13 @@
-from ..app import apppass
+from ..app import app
+import requests
 from flask import render_template
 
-@app.route("/")
+@app.route("/retrieve_wikidata/<string:id>")
 def retrieve_wikidata(id):
-    pass
-    # return render_template("pages/pays.html", pays=nom)
+    base_url = "https://www.wikidata.org/wiki/Special:EntityData" # FIXME à mettre ailleurs ?
+    response = requests.get(f"{base_url}/{id}.json")
+    # Création des 3 objets appelés dans le template HTML grâce à Jinja
+    status_code = response.status_code
+    content = response.headers.get("Content-Type")
+    json = response.json()
+    return render_template("pages/display_wikidata_content.html", id=id, status_code = status_code, content=content, json=json)
