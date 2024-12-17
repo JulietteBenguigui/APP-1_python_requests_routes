75dd796a6c6fd5c2d274b01c4a63a8672f1cab42
Juliette Benguigui <juliette.benguigui@hotmail.fr>
2024-12-17 11:19:02 +0100

    Ajout clause if

--- app/routes/generales.py
+++ app/routes/generales.py
@@ -9,5 +9,6 @@ def retrieve_wikidata(id):
     # Création des 3 objets appelés dans le template HTML grâce à Jinja
     status_code = response.status_code
     content = response.headers.get("Content-Type")
+    check_type = type(response.json())
     json = response.json()
-    return render_template("pages/display_wikidata_content.html", id=id, status_code = status_code, content=content, json=json)
+    return render_template("pages/display_wikidata_content.html", id=id, status_code = status_code, content=content, json=json, check_type=check_type)
